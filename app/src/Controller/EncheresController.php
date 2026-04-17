a59244aa51b240cce1e9f4c1124881b3c9c2ae97
MohandBir <birmoho@gmail.com>
2026-04-17 14:44:35 +0200

    ticket2: page show

--- app/src/Controller/EncheresController.php
+++ app/src/Controller/EncheresController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Item;
 use App\Repository\ItemRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,6 +16,14 @@ final class EncheresController extends AbstractController
     {
         return $this->render('encheres/index.html.twig', [
             'items' => $itemRepo->findAll(),
+        ]);
+    }
+ 
+    #[Route('/show/{id}', name: 'app_encheres_show')]
+    public function show( Item $item,EntityManagerInterface $em): Response
+    {
+        return $this->render('encheres/show.html.twig', [
+            'item' => $item,
         ]);
     }
 }
