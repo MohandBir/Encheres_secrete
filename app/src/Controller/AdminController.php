e46aef04374f2de26c1a0f6123cf2c2f199614a4
MohandBir <birmoho@gmail.com>
2026-04-20 17:26:27 +0200

    ticket6: fonctionnalité - changer le status

--- app/src/Controller/AdminController.php
+++ app/src/Controller/AdminController.php
@@ -10,13 +10,14 @@ use App\Repository\ItemRepository;
 use App\Repository\OfferRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AdminController extends AbstractController
 {
     #[Route('/admin/{id<[0-9]+>?null}', name: 'app_admin_index')]
-    public function index(?Category $category, ItemRepository $itemRepo, CategoryRepository $categoryRepo, EntityManagerInterface $em): Response
+    public function index(?Category $category, ItemRepository $itemRepo, CategoryRepository $categoryRepo): Response
     {
         $id = $category ? (int) $category->getId() : null;
         $items = $itemRepo->findByCategoryWithOffers($id);
@@ -27,7 +28,7 @@ final class AdminController extends AbstractController
     }
 
     #[Route('/admin/show/{id<[0-9]+>}', name: 'app_admin_show')]
-    public function show($id, ItemRepository $itemRepo, OfferRepository $offerRepo, EntityManagerInterface $em): Response
+    public function show($id, ItemRepository $itemRepo, OfferRepository $offerRepo): Response
     {
         $item = $itemRepo->find($id);
         $offer = $offerRepo->findOneBy(['item' => $item]);
@@ -38,5 +39,34 @@ final class AdminController extends AbstractController
             'item' => $item ,
         ]);
 
+    } 
+
+    #[Route('/admin/change-status/{id}', name: 'app_admin_change_status')]
+    public function changeStatus(Item $item, OfferRepository $offerRepo, EntityManagerInterface $em, Request $request): Response
+    {
+        $offer = $offerRepo->findOneBy(['item' => $item]);
+
+        $submitedToken = $request->getPayload()->get('token');
+        if($this->isCsrfTokenValid('change', $submitedToken)) {
+            if ($item->getStatus() === 'unpublished') {
+                $item->setStatus('published');
+                $em->flush();
+                $this->addFlash('success', 'L\'enchère est publié avec succès');
+     
+                return $this->redirectToRoute('app_admin_index');
+            }
+            if ($item->getStatus() === 'published' && !$offer) {
+                $item->setStatus('unpublished');
+                $em->flush();
+                $this->addFlash('success', 'L\'enchère remise à Non publiée');
+     
+                return $this->redirectToRoute('app_admin_index');
+            } else {
+                $this->addFlash('danger', 'L\'enchère possède des enréchisseur!!');
+     
+                return $this->redirectToRoute('app_admin_show', ['id' => $item->getId()]);
+            }
+
+        }
     }
 }
